ea28965ecf5786dd20f6e4e62efc1e07ce529a3c
marcbaguion <108505608+marcbaguion@users.noreply.github.com>
2022-07-10 19:06:17 +0800

    Milestone 3: Add Event Listeners

--- app/src/main/java/com/example/backgroundbaguion/MainActivity.java
+++ app/src/main/java/com/example/backgroundbaguion/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.backgroundbaguion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButtonListenerMethod();
+        ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.clMain);
+        bgElement.setBackgroundColor(Color.RED);
+    }
+    public void myButtonListenerMethod() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.clMain);
+                int color =((ColorDrawable) bgElement.getBackground()).getColor();
+                if (color == Color.RED) {
+                    bgElement.setBackgroundColor(Color.BLUE);
+                } else {
+                    bgElement.setBackgroundColor(Color.RED);
+                }
+            }
+        });
+
     }
 }
